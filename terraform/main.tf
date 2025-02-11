af366dbf098ff9863555ec7d9ced9b84888abf9c
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-02-12 00:02:10 +0200

    another

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,27 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "af-south-1"
 }
 
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "default_vpc"
+  }
+}
+
+data "aws_availability_zones" "available_zones" {
+  
+}
+
+resource "aws_default_subnet" "subnet_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+}
+
+resource "aws_default_subnet" "subnet_az2" {
+  availability_zone = data.aws_availability_zones.available_zones.names[1]
+}
 
 resource "aws_db_instance" "beantradedb" {
   identifier             = "beantradedb"
