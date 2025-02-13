e6a1220945a0894366b8e4f1ee7326886d8beb2c
Shashin Gounden <shashin.gounden@bbd.co.za>
2025-02-13 13:38:17 +0200

    Testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,18 +29,6 @@ resource "aws_default_subnet" "subnet_az2" {
   availability_zone = data.aws_availability_zones.available_zones.names[1]
 }
 
-
-resource "aws_security_group" "allow_mssql" {
-  name        = "allow_mssql"
-
-  ingress {
-    from_port   = 1433
-    to_port     = 1433
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_db_instance" "beantradedb" {
   identifier             = "beantradedb"
   engine                 = "sqlserver-ex"
@@ -52,6 +40,7 @@ resource "aws_db_instance" "beantradedb" {
   username               = var.db_username
   password               = var.db_password
   skip_final_snapshot    = true
+  db_name                = "beantradedb"
 
   tags = {
     Name = "beantradedb"
