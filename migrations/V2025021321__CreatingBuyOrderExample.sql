f9dd5a34aa7237ca1b09b94cc2f30d580001d733
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2025-02-15 11:37:31 +0200

    Update vars and migrations

--- migrations/V2025021321__CreatingBuyOrderExample.sql
+++
@@ -1,5 +0,0 @@
--- Create some active orders
-INSERT INTO Orders (UserID, BeanID, CurrencyCodeId, OrderTypeId, StatusId, PricePerBean, Quantity, RemainingQuantity)
-VALUES 
-	  (1, 2, 1, 1, 1, 5.00, 200, 200);    -- User 1 wants to buy 200 Robusta at $5 each
-GO
